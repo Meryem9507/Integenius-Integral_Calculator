25c21c5ce68b5e77846f3d95ded44a628cfacd58
Meryem aka Meru <ongenmeryem@gmail.com>
2025-08-05 21:17:42 +0300

    c

--- integenius_app/android/app/build.gradle.kts
+++ integenius_app/android/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "com.integenius"
         minSdk = 21
         targetSdk = 35 
-        versionCode = 9
+        versionCode = 10
         versionName = "1.0.0"
     }
 
