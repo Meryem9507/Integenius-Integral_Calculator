c157ce9fcee8e81a55a7358140a3c80a87f96cb0
Meryem aka Meru <ongenmeryem@gmail.com>
2025-08-10 13:57:12 +0300

    Update app.py

--- FlaskAPI/app.py
+++ FlaskAPI/app.py
@@ -50,10 +50,10 @@ def token_required(f):
     return decorated
 
 limiter = Limiter(
-    app,
     key_func=get_remote_address,
     default_limits=["100 per hour"]  
 )
+limiter.init_app(app)
  
 
 @app.route('/')
